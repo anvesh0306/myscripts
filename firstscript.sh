8efaaab956b51a93b2dfe8ab17656f2fb2060843
anvesh0306 <anveshaus2020@gmail.com>
2026-05-21 14:58:21 +1000

    echo script

---
+++ firstscript.sh
@@ -0,0 +1,6 @@
+!#/bin/bash
+echo "Hello World"
+echo "This is my first script"
+echo "I am learning shell scripting"
+echo "I am excited to learn more about it"
+echo "I will practice writing more scripts"
